5ecc70cf8cd3f360cbe186d56b5ba895fb5c6593
kc <xkylechan@gmail.com>
2025-04-05 22:21:16 -0600

    players can pick a spot to place marker VIA prompt.  MArker will be placed if the spot if empty.  If spot is occupied, will ask user for spot again

--- script.js
+++ script.js
@@ -7,3 +7,51 @@ const gameBoard = (function () {
 
   return { board };
 })();
+
+const displayController = (function () {
+  const displayBoard = () => {
+    console.log(`        ${gameBoard.board[0]} | ${gameBoard.board[1]} | ${gameBoard.board[2]}
+      ----------
+        ${gameBoard.board[3]} | ${gameBoard.board[4]} | ${gameBoard.board[5]}
+      ----------
+        ${gameBoard.board[6]} | ${gameBoard.board[7]} | ${gameBoard.board[8]}`);
+  };
+
+  return { displayBoard };
+})();
+
+const gameController = (function () {
+  let maxTurns = 9;
+  let playerOne = 'X';
+  let playerTwo = 'O';
+
+  function playGame() {
+    for (let i = 1; i <= maxTurns; i++) {
+      let spotPicked = getPickedSpot();
+
+      if (i % 2 == 0) {
+        gameBoard.board[spotPicked] = playerOne;
+      } else {
+        gameBoard.board[spotPicked] = playerTwo;
+      }
+      displayController.displayBoard();
+
+      checkWinner();
+    }
+  }
+
+  function getPickedSpot() {
+    while (true) {
+      let getSpot = parseInt(prompt('pick a spot: '));
+
+      if (gameBoard.board[getSpot] == '') {
+        console.log('hi');
+        return getSpot;
+      }
+    }
+  }
+
+  function checkWinner() {}
+
+  // playGame();
+})();
